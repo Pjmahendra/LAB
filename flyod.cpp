87c7ffdc304617578c668d48116ad5202ceab256
Mahendra Pj <pjmahendravk@gmail.com>
2025-11-10 06:07:05 -0800

    Add files via upload

---
+++ flyod.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define INF 99999  
+
+int main() {
+    int n;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+    vector<vector<int>> dist(n, vector<int>(n));
+    cout << "\nEnter the weight matrix (use " << INF << " for no edge):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+        }
+    }
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    cout << "\nShortest distance matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
